8f7e24da8e3bce4120c4b4c6f0c3127110059644
cm8908 <jungminseob2@gmail.com>
2023-05-02 11:17:19 +0900

    diffusion.py - generation

--- diffusion.py
+++ diffusion.py
@@ -1,3 +1,6 @@
+import os
+import h5py
+from utils import ensure_dir
 from dataset.lgan_dataset import get_dataloader
 from config.configDiffusion import ConfigDiffusion
 from trainer.trainerDiffusion import TrainerGaussianDiffusion1D
@@ -14,4 +17,15 @@ if not cfg.test:
 
     agent.train(train_loader)
 else:
-    raise NotImplementedError()
+    # load trained weights
+    agent.load_ckpt(cfg.ckpt)
+
+    # run generator
+    generated_shape_codes = agent.generate(cfg.n_samples)
+
+    # save generated z
+    save_path = os.path.join(cfg.exp_dir, "results/fake_z_ckpt{}_num{}.h5".format(cfg.ckpt, cfg.n_samples))
+    
+    ensure_dir(os.path.dirname(save_path))
+    with h5py.File(save_path, 'w') as fp:
+        fp.create_dataset("zs", shape=generated_shape_codes.shape, data=generated_shape_codes)
