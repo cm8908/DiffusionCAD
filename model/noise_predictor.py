8aaa9ae4b145be209e101ae68696cc4ae6250395
cm8908 <jungminseob2@gmail.com>
2023-05-25 15:36:38 +0900

    unet add

--- model/noise_predictor.py
+++ model/noise_predictor.py
@@ -1,30 +1,8 @@
 import torch
 import torch.nn as nn
 import math
+from .nn import SiLU, timestep_embedding
 
-class SiLU(nn.Module):
-    def forward(self, x):
-        return x * torch.sigmoid(x)
-
-def timestep_embedding(timesteps, dim, max_period=10000):
-    """
-    Create sinusoidal timestep embeddings.
-
-    :param timesteps: a 1-D Tensor of N indices, one per batch element.
-                      These may be fractional.
-    :param dim: the dimension of the output.
-    :param max_period: controls the minimum frequency of the embeddings.
-    :return: an [N x dim] Tensor of positional embeddings.
-    """
-    half = dim // 2
-    freqs = torch.exp(
-        -math.log(max_period) * torch.arange(start=0, end=half, dtype=torch.float32) / half
-    ).to(device=timesteps.device)
-    args = timesteps[:, None].float() * freqs[None]
-    embedding = torch.cat([torch.cos(args), torch.sin(args)], dim=-1)
-    if dim % 2:
-        embedding = torch.cat([embedding, torch.zeros_like(embedding[:, :1])], dim=-1)
-    return embedding
 
 class TransformerEncoder(nn.Module):
     def __init__(self, h_dim, ff_dim, num_layers, norm_type, dropout_rate, num_heads):
